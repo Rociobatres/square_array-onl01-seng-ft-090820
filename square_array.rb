134fe84948a782344761b0daf578c6698e746ccb
Rocio Batres <jonasgirl21@gmail.com>
2020-09-13 17:43:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 numbers = [1,2,3]
 
 def square_array(array)
-    new_number = []
-    array.each do |
+  new_number = []
+    array.each do |digit| ** 2 
     new_number << "new number"
-  end   
+    end   
 end
